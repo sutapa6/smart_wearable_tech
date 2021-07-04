73a1a368e7cde427272662eeb07d3f8361219dbe
Taylor Chan <taylorchan.0119@gmail.com>
2021-07-04 19:18:51 +1000

    chore: copy csv_format from jupter notebook into src/format_csv.py

--- src/format_csv.py
+++ src/format_csv.py
@@ -1,98 +1,43 @@
-
 import pandas as pd
 
-
-def csv_format(filename: str):
+def csv_format(lab_num: int, scope_num: int):
     '''
-        Takes a csv file and formats it in such a way that can be 
-        plotted using the matplotlib library. Essentially, this is changing 
-        each header to not include hyphens (such as x-axis) or plain integers
-        It also deletes any rows with empty values 
-
-        Arguments:
-            filename (str)    - name of the csv file to be plotted
-
-        Exceptions:
-            InputError  - Occurs when   1) filename is not a string
-                                        2) filename does not exist
-                                        3) filename is not a csv file
-
-        Return Value:
-            None 
+    Format oscilloscope files for easier manipulation and plotting
+    Inputs:
+        lab_num (int)       Lab number of the scope to be formatted
+        scope_num (int)     scope number of the file to be formatted
+    Outputs:
+        csv file, formatted inplace
     '''
-    f = pd.read_csv(filename)
-    f = f.rename(columns=({'x-axis': 'x_axis'}))
-    f = f.rename(columns=({'2': 'channel_2'}))
-    f = f.drop([0], axis=0)
 
-    # Removes rows containing empty values
-    i = 1
-    while f["channel_2"][i] != f["channel_2"][i]:
-        f = f.drop([i], axis=0)
-        i += 1
+    # Load csv file
+    df = pd.read_csv(f'data/Lab_{lab_num}/Raw/scope_{scope_num}.csv')
+
+    # Rename columns
+    cols = list(df.columns)
+    for header in cols:
+        if 'axis' in header:
+            df.rename({header: 'time'}, axis=1, inplace=True)
+        elif 'channel' not in header:
+            df.rename({header: 'channel_' + header}, axis=1, inplace=True)
+
+    # Delete row with units (first row)
+    df = df.drop(0)
     
-    # Convert negative timestamps
-    t = f['x_axis'][1]
-    if t < 0:
-        for i in range(1, len(f['x_axis']) +1):
-            f['x_axis'][i] += abs(t)
-    
+    # Delete completley empty rows, and remove indexes from the csv
+    df = df.dropna(how='all')
+    df = df.reset_index(drop=True)
 
-    keep_col = ['x_axis', 'channel_2']
-    new_f = f[keep_col]
+    # Convert from scientific to float
+    cols = list(df.columns)
+    for header in cols:
+        df[header].apply(lambda x: '%.7f' % x)
 
-    # Overwriting existing csv file with formatted csv
-    new_f.to_csv(filename, index=False)
+    # Correct any negative timestamps
+    t0 = df['time'][0]
+    if t0 < 0:
+        df['time'] = df['time'].astype(float) - t0
 
-    pass
 
-
-def csv_format(filename: str):
-    '''
-        Takes a csv file and formats it in such a way that can be 
-        plotted using the matplotlib library. Essentially, this is changing 
-        each header to not include hyphens (such as x-axis) or plain integers
-        It also deletes any rows with empty values 
-
-        Arguments:
-            filename (str)    - name of the csv file to be plotted
-
-        Exceptions:
-            InputError  - Occurs when   1) filename is not a string
-                                        2) filename does not exist
-                                        3) filename is not a csv file
-
-        Return Value:
-            None 
-    '''
-    f = pd.read_csv(filename)
-    keep_col = ['x_axis', 'channel_2']
-    new_f = f[keep_col]
-    new_f.to_csv(filename, index=False)
-    '''
-    r = csv.reader(open(filename))
-    lines = list(r)
-
-    # 1) Convert header to names that matplotlib will like
-    lines[0][0] = 'x_axis'
-    lines[0][1] = 'channel_2'
-
-    # 2) Remove second row (second, volt, volt)
-    del lines[1:3]
-
-    # 3 Change Scientific Notation to decimal
-    for row in lines:
-        if row[0] == '' and row[1] == '' and row[2] == '':
-            del row
-
-        if row[0] != "x_axis":
-            row[0] = float(row[0])
-
-        if row[1] != "channel_2" and row[1] != '':
-            row[1] = float(row[1])
-
-    # Writes new formatted csv to the file, ready for plotting
-    writer = csv.writer(open(filename, 'w'))
-    writer.writerows(lines)
-    '''
-    pass
+    # Save modified csv to a new file, display first 10 rows
+    df.to_csv(f'data/Lab_{lab_num}/modified_scope_{scope_num}.csv', index=False)
