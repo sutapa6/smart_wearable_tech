8014f63a262ff4f0c96a74073c8f1def1b5cfe53
Taylor Chan <taylorchan.0119@gmail.com>
2021-07-04 23:23:34 +1000

    feat: cleaner formatting, importing jupyter notebooks

--- src/format_csv.py
+++ src/format_csv.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-def csv_format(lab_num: int, scope_num: int):
+def csv_format(lab_num: int, scope_num: int, sensor_ch: int):
     '''
     Format oscilloscope files for easier manipulation and plotting
     Inputs:
         lab_num (int)       Lab number of the scope to be formatted
         scope_num (int)     scope number of the file to be formatted
     Outputs:
-        csv file, formatted inplace
+        save_path (str)     path to the formatted file from the root directory
     '''
 
     # Load csv file
@@ -17,9 +17,11 @@ def csv_format(lab_num: int, scope_num: int):
     cols = list(df.columns)
     for header in cols:
         if 'axis' in header:
-            df.rename({header: 'time'}, axis=1, inplace=True)
-        elif 'channel' not in header:
-            df.rename({header: 'channel_' + header}, axis=1, inplace=True)
+            df.rename({header: 'Time'}, axis=1, inplace=True)
+        elif str(sensor_ch) in header:
+            df.rename({header: 'Sensor'}, axis=1, inplace=True)
+        else:
+            df = df.drop(columns=[header])
 
     # Delete row with units (first row)
     df = df.drop(0)
@@ -34,10 +36,26 @@ def csv_format(lab_num: int, scope_num: int):
         df[header].apply(lambda x: '%.7f' % x)
 
     # Correct any negative timestamps
-    t0 = df['time'][0]
+    t0 = df['Time'][0]
     if t0 < 0:
-        df['time'] = df['time'].astype(float) - t0
+        df['Time'] = df['Time'].astype(float) - t0
 
 
     # Save modified csv to a new file, display first 10 rows
-    df.to_csv(f'data/Lab_{lab_num}/modified_scope_{scope_num}.csv', index=False)
+    save_path = f'data/Lab_{lab_num}/Formatted/scope_{scope_num}.csv'
+    df.to_csv(save_path, index=False)
+
+    return str(save_path)
+
+if __name__ == '__main__':
+    # Specify the data to be formatted
+    lab_num = 1
+    scope_num = 5
+    sensor_ch = 2
+
+    # Call csv_format, display the first 10 rows of the formatted file
+    filename = csv_format(lab_num, scope_num, sensor_ch)
+    print(filename, type(filename))
+
+    formatted = pd.read_csv(filename)
+    formatted.head(10)
